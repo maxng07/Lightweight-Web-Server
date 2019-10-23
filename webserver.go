1940b4fc5ee75e216f9f02ae6854b55e3b1d6831
Max Ng <45160135+maxng07@users.noreply.github.com>
2019-10-24 00:03:32 +0800

    Add files via upload

---
+++ webserver.go
@@ -0,0 +1,53 @@
+// A basic HTTP server.
+// By default, it serves the current working directory on port 8080.
+package main
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+	"os"
+
+)
+
+var (
+	ConfigFile	= "config.json"
+	Address		= ""
+	Port		= ""
+	Dir 		= ""
+	Tmpfile		= ""		
+	c 			Config
+)
+
+// struct for loading configuration information
+type Config struct {
+
+    Address             string
+    Port	            string
+    Dir 				string
+    Tmpfile				string
+}
+
+func init() {
+ // open our config and parse
+   confBytes, e := ioutil.ReadFile(ConfigFile)
+    if e != nil {
+        fmt.Println(e.Error())
+        os.Exit(1)
+    }
+
+    if e := json.Unmarshal(confBytes, &c); e != nil {
+        fmt.Println(e.Error())
+        os.Exit(2)
+    }
+
+}
+
+func main() {
+	listen := c.Address + ":" + c.Port
+	log.Printf("listening on %q...", listen)
+	err := http.ListenAndServe(listen, http.FileServer(http.Dir(c.Dir)))
+	log.Fatalln(err)
+}
